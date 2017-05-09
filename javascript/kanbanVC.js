41585430f4589c65ee2f92af0b643f432c365a1d
ktyminski <karoltyminskikarol@gmail.com>
2017-05-10 00:38:05 +0200

    final commit, fixed sortable

--- javascript/kanbanVC.js
+++ javascript/kanbanVC.js
@@ -1,82 +1,63 @@
 var materialVC = {};
 
-
 materialVC.initView = function() {
 
-materialVC.Tasks = [];
-
-var actualtasklist;
-var actualobject;
-var lastclicked;
-var context;
+  materialVC.Tasks = [];
+  var $modalContent =  $(".modalbox__content");
+  var actualtasklist;
+  var actualobject;
+  var lastclicked;
+  var context;
 
     $(".modalbox__datepicker" ).datepicker();
 
-    $(".draggable").draggable({
-        cursor: 'move',
-        revert: 'invalid',
-        distance: 20
-    });
+    $(".items").sortable({
+           connectWith: ".items",
+           receive: function(event, ui) {
+             var search;
+             var changelist;
+             search = $(this).find("h5").text();
+             materialVC.searchArray(search);
+             changelist = $(this).prop("id");
+             materialVC.updateTaskList(changelist);
+             $(ui.helper).remove();
+           }
+       });
 
-    $(".droppable").droppable({
-        hoverClass: 'ui-state-active',
-        tolerance: 'pointer',
-        accept: function (event, ui) {
-            return true;
-        },
-        drop: function (event, ui) {
-            var obj;
-            if ($(ui.helper).hasClass('draggable')) {
-                if($(ui.draggable).parents(".editor-container").length > 0){
-                    $(ui.helper).clone().appendTo($(this)).removeAttr('style');
-                    $(ui.helper).remove();
-                }else{
-                    obj = $(ui.helper);
-                    obj.removeClass('droppable');
-                    obj.addClass('editable').removeAttr('style');
-                    $(this).append(obj);
-                }
-            }
-        }
-    }).sortable({
-        revert: false
-    });
-
-  $(".tasks__newtask-button").click(function(){
+  $(".tasks__newtask-button").unbind("click").click(function(){
     $(".modalbox").fadeIn();
-    actualtasklist = $(this).closest(".tasks").find(".tasks__list").attr('id');
+    actualtasklist = $(this).closest(".tasks").find(".tasks__list").attr("id");
     lastclicked = "new";
   });
 
-  $(".modalbox__savebtn").click(function(){
+  $(".modalbox__savebtn").unbind("click").click(function(){
     var task = materialVC.getModalInputs();
     var updatingtask;
     if (lastclicked === "new"){
       updatingtask = new materialVC.Task(task);
-      materialVC.Addtask(updatingtask);
+      materialVC.addTask(updatingtask);
     } else{
-      materialVC.Updatetask(task,actualobject);
+      materialVC.updateTask(task,actualobject);
     }
-    materialVC.Hidemodal();
+    materialVC.hideModal();
   });
 
-  $(".modalbox__cancelbtn").click(function(){
-      materialVC.Hidemodal();
+  $(".modalbox__cancelbtn").unbind("click").click(function(){
+      materialVC.hideModal();
   });
 
-
-  $(document).on('dblclick','.tasks__list-task',function(){
+  $(document).unbind("dblclick").on("dblclick",".tasks__list-task",function(){
+    var searchTask;
     context = $(this);
-    actualtasklist = $(this).closest(".tasks").find(".tasks__list").attr('id');
-    var searchTask = $(this).find("h5").text();
+    actualtasklist = $(this).closest(".tasks").find(".tasks__list").attr("id");
+    searchTask = $(this).find("h5").text();
     materialVC.searchArray(searchTask);
     lastclicked = "update";
     materialVC.setModalInputs(actualobject);
     $(".modalbox").fadeIn();
   });
 
-
-  $(".tasks__buttonshow").click(function(){
+  $(".tasks__buttonshow").unbind("click").click(function(){
     $(this).closest( ".tasks" ).children(".tasks__list").toggle(150);
     $(this).toggleClass("tasks__buttonhide");
   });
@@ -92,24 +73,24 @@ var context;
   materialVC.setModalInputs = function(recieved_task){
     for (var i = 0; i < 4; i++) {
       if (i === Number(recieved_task.label)){
-        $(".modalbox__content").find("[type=radio]").eq(i).prop('checked', true);
+      $modalContent.find("[type=radio]").eq(i).prop("checked", true);
       }
     }
-    $(".modalbox__content").find('[data-name="name"]').val(recieved_task.name);
-    $(".modalbox__content").find('[data-name="tags"]').val(recieved_task.tags);
-    $(".modalbox__content").find('[data-name="starts"]').val(recieved_task.start);
-    $(".modalbox__content").find('[data-name="finishes"]').val(recieved_task.finish);
-    $(".modalbox__content").find('[data-name="description"]').val(recieved_task.description);
+    $modalContent.find('[data-name="name"]').val(recieved_task.name);
+    $modalContent.find('[data-name="tags"]').val(recieved_task.tags);
+    $modalContent.find('[data-name="starts"]').val(recieved_task.start);
+    $modalContent.find('[data-name="finishes"]').val(recieved_task.finish);
+    $modalContent.find('[data-name="description"]').val(recieved_task.description);
   };
 
-  materialVC.Addtask = function(newtask) {
+  materialVC.addTask = function(newtask) {
     materialVC.Tasks.push(newtask);
     $('#'+newtask.actualtasklist).append("<div class='tasks__list-task draggable'><h5 class='task__text'>"+ newtask.name +"</h5><p class='task__tags'>"+ newtask.tags +"</p></div>");
     context = $('#'+newtask.actualtasklist).children().last();
-    materialVC.showlabel(newtask);
+    materialVC.showLabel(newtask);
   };
 
-  materialVC.Updatetask = function(task,actualobject) {
+  materialVC.updateTask = function(task,actualobject) {
      $(context).find("h5").text(task.name);
      $(context).find("p").text(task.tags);
      actualobject.name = task.name;
@@ -118,20 +99,24 @@ var context;
      actualobject.start = task.start;
      actualobject.finish = task.finish;
      actualobject.description = task.description;
-     materialVC.showlabel(task);
+     materialVC.showLabel(task);
   };
 
-  materialVC.showlabel = function(updatedtask){
+  materialVC.updateTaskList = function (changelist) {
+    actualobject.actualtasklist = changelist;
+  };
+
+  materialVC.showLabel = function(updatedtask){
     if (updatedtask.label === "0"){$(context).removeClass().addClass("tasks__list-task draggable tasks__labels-red");}
     if (updatedtask.label === "1"){$(context).removeClass().addClass("tasks__list-task draggable tasks__labels-blue");}
     if (updatedtask.label === "2"){$(context).removeClass().addClass("tasks__list-task draggable tasks__labels-green");}
     if (updatedtask.label === "3"){$(context).removeClass().addClass("tasks__list-task draggable tasks__labels-orange");}
   };
 
-  materialVC.Hidemodal = function(){
+  materialVC.hideModal = function(){
     $(".modalbox").hide();
     $(".form-control").val("");
-    $("input:radio").prop('checked', false);
+    $("input:radio").prop("checked", false);
   };
 
   materialVC.Task = function(task){
@@ -145,21 +130,20 @@ var context;
   };
 
   materialVC.getModalInputs = function(){
-    var checkedlabel;
-    if($("input:radio").is(':checked')) {
+    var checkedlabel,task;
+    if($("input:radio").is(":checked")) {
       checkedlabel = $("input:radio:checked").val();
     }
-    var task = {
-      name : $(".modalbox__content").find('[data-name="name"]').val(),
-      tags : $(".modalbox__content").find('[data-name="tags"]').val(),
+    task = {
+      name : $modalContent.find('[data-name="name"]').val(),
+      tags : $modalContent.find('[data-name="tags"]').val(),
       label : checkedlabel,
-      start : $(".modalbox__content").find('[data-name="starts"]').val(),
-      finish : $(".modalbox__content").find('[data-name="finishes"]').val(),
-      description : $(".modalbox__content").find('[data-name="description"]').val()
+      start : $modalContent.find('[data-name="starts"]').val(),
+      finish : $modalContent.find('[data-name="finishes"]').val(),
+      description : $modalContent.find('[data-name="description"]').val()
     };
     return task;
   };
-
 };
 
 materialVC.initView();
